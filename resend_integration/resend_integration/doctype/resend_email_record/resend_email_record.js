36da8af25f345852eefa4b1d8324be08686b4bdd
Hussain Nagaria <hussainbhaitech@gmail.com>
2023-06-28 16:49:12 +0530

    feat: integrate with resend API

---
+++ resend_integration/resend_integration/doctype/resend_email_record/resend_email_record.js
@@ -0,0 +1,6 @@
+// Copyright (c) 2023, Hussain Nagaria and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Resend Email Record", {
+//     refresh(frm) {},
+// });
---
+++ resend_integration/resend_integration/doctype/resend_email_record/resend_email_record.py
@@ -0,0 +1,29 @@
+# Copyright (c) 2023, Hussain Nagaria and contributors
+# For license information, please see license.txt
+
+import frappe
+import resend
+from frappe.model.document import Document
+from frappe.utils.password import get_decrypted_password
+
+def get_resend_api_key():
+	return get_decrypted_password("Resend Settings", "Resend Settings", "api_key")
+
+resend.api_key = get_resend_api_key()
+
+class ResendEmailRecord(Document):
+	def before_submit(self):
+		if self.status == "Sent":
+			frappe.throw("Email has already been sent.")
+
+		email = resend.Emails.send({
+			"from": self.from_email,
+			"to": self.to_emails.strip().split(","),
+			"subject": self.subject,
+			"html": self.email_html
+		})
+
+		self.status = "Sent"
+		self.resend_id = email["id"]
+
+
