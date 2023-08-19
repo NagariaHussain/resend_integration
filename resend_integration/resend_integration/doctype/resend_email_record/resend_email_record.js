6d797021fa25cb5f35802fc8c54a9d5a618b7640
Hussain Nagaria <hussainbhaitech@gmail.com>
2023-08-19 14:28:20 +0530

    feat: Email HTML Preview

--- resend_integration/resend_integration/doctype/resend_email_record/resend_email_record.js
+++ resend_integration/resend_integration/doctype/resend_email_record/resend_email_record.js
@@ -1,6 +1,13 @@
 // Copyright (c) 2023, Hussain Nagaria and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Resend Email Record", {
-//     refresh(frm) {},
-// });
+frappe.ui.form.on("Resend Email Record", {
+  refresh(frm) {
+    let email_html = cur_frm.doc.email_html;
+    if (email_html) {
+      const label_html = `<label class="control-label mb-1" style="padding-right: 0px;">HTML Preview</label>`;
+      email_html = label_html + email_html;
+      cur_frm.get_field("html_preview").$wrapper.html(email_html);
+    }
+  },
+});
